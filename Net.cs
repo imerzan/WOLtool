2af6edc0443e22d9a108932075c865c5247ab44c
Steve Tolzmann <steven.tolzmann@gmail.com>
2020-06-28 19:24:37 -0500

    Add project files.

---
+++ Net.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WOLtool
+{
+    public class Net
+    {
+        public static int WakeOnLan(string broadcastip, string macaddress)
+        {
+            try
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPAddress ip = IPAddress.Parse(broadcastip);
+                IPEndPoint ep1 = new IPEndPoint(ip, 7); // Port 7 common WOL port
+                IPEndPoint ep2 = new IPEndPoint(ip, 9); // Port 9 common WOL port
+                byte[] mp = BuildMagicPacket(macaddress);
+                if (mp == null)
+                {
+                    throw new NullReferenceException("MagicPacket value is null, please check MAC Address.");
+                }
+                sock.SendTo(mp, ep1);
+                sock.SendTo(mp, ep2);
+                sock.Close();
+                Console.WriteLine("Success!");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return -1;
+            }
+        }
+        private static byte[] BuildMagicPacket(string macAddress) // MacAddress in any standard HEX format
+        {
+            try
+            {
+                macAddress = Regex.Replace(macAddress, "[: -]", "");
+                byte[] macBytes = new byte[6];
+                for (int i = 0; i < 6; i++)
+                {
+                    macBytes[i] = Convert.ToByte(macAddress.Substring(i * 2, 2), 16);
+                }
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (BinaryWriter bw = new BinaryWriter(ms))
+                    {
+                        for (int i = 0; i < 6; i++)  //First 6 times 0xff
+                        {
+                            bw.Write((byte)0xff);
+                        }
+                        for (int i = 0; i < 16; i++) // then 16 times MacAddress
+                        {
+                            bw.Write(macBytes);
+                        }
+                    }
+                    return ms.ToArray(); // 102 bytes magic packet
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WOLtool
+{
+    class Program
+    {
+        private static string szBroadcastIP, szMacAddress;
+        static int Main(string[] args)
+        {
+            if (args.Length == 0) // No args, get user input
+            {
+                Console.Write("Enter Broadcast IP: ");
+                szBroadcastIP = Console.ReadLine();
+                Console.Write("Enter MAC Address: ");
+                szMacAddress = Console.ReadLine();
+                return Net.WakeOnLan(szBroadcastIP, szMacAddress);
+            }
+            else if (args.Length == 2) // arg1 = Broadcast IP, arg2 = MAC Address
+            {
+                return Net.WakeOnLan(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Unexpected number of arguments.");
+                return -1;
+            }
+        }
+    }
+}
